03220fb63086a9ff2597bf469888941a1abbe2d2
aerrojudilip <aerrojudilip@gmail.com>
2025-09-16 12:32:12 -0500

    initial

---
+++ Rafcon/RafconTool.bat
@@ -0,0 +1,13 @@
+@echo off
+rem ----------------------------------------------------------------------------
+rem    Licensed Materials - Property of IBM
+rem   
+rem  
+rem    5725-D51, 5725-D52, 5725-D53, 5725-D54
+rem  
+rem    (c) Copyright IBM Corporation 2014 - 2023
+rem  
+rem    US Government Users Restricted Rights- Use, duplication or
+rem    disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+rem ----------------------------------------------------------------------------
+java -Xms64m -Xmx2048m -cp .;./Rafcon.jar;./lib/dom4j.jar;./lib/poi-ooxml-full-5.2.2.jar;./lib/commons-collections4-4.3.jar;./lib/commons-compress-1.21.jar;./lib/commons-math3-3.6.1.jar;./lib/SparseBitSet-1.2.jar;./lib/poi-5.2.2.jar;./lib/poi-ooxml-5.2.2.jar;./lib/poi-ooxml-lite-5.2.2.jar;./lib/xmlbeans-5.0.2.jar;./lib/json4j.jar;.lib/jackson-core-2.13.2.jar;.lib/jackson-databind-2.13.2.jar;.lib/jackson-annotations-2.13.2.jar;./lib/aurora-sdk-client.jar;./lib/log4j-api-2.17.1.jar;./lib/commons-io-2.10.0.jar;./properties com.openpages.css.rafcon.Main
